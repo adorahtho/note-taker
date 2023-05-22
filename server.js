f68968b42e6bfd2a249b150748c7e553fae68ab8
thoak90codes <adorahtho@gmail.com>
2023-05-21 23:13:57 -0500

    added unique id to new note

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
+// Helper method for generating unique ids
+const uuid = require('./helpers/uuid')
 
-const PORT = process.env.PORT || 3001;
-
+const PORT = 3001;
 const app = express();
 
 app.use(express.json());
@@ -11,6 +12,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
 
+//GET request to read db.json file and return saved noted as JSON.
 app.get('/api/notes', (req, res) => {
   fs.readFile('./db/db.json', 'utf8', (err, data) => {
     if (err) {
@@ -22,23 +24,37 @@ app.get('/api/notes', (req, res) => {
   });
 });
 
+//New note received to save to request body, add to db.json file, then return to client with unique id.
 app.post('/api/notes', (req, res) => {
   const file = './db/db.json'
-  const content = req.body
-  fs.readFile(file, 'utf8', (err, data) => {
-    if (err) {
-      console.error(err);
-    } else {
-      const parsedData = JSON.parse(data);
-      parsedData.push(content);
-      fs.writeFile(file, JSON.stringify(parsedData), (err) => {
-        err ? console.error(err) : console.info(`\nData written to ${file}`)
-        res.json(parsedData)
-      }
-      );
-      }
+  const {title, text} = req.body
 
-  });
+  if (title && text){
+    const newNote = {
+      title,
+      text,
+      id: uuid(),
+    }
+
+    //Obtain existing notes
+    fs.readFile(file, 'utf8', (err, data) => {
+      if (err) {
+        console.error(err);
+      } else {
+        const parsedData = JSON.parse(data);
+
+        //Add new note
+        parsedData.push(newNote);
+
+        //Write updated notes back to db.json file
+        fs.writeFile(file, JSON.stringify(parsedData), (err) => {
+          err ? console.error(err) : console.info(`\nData written to ${file}`)
+          res.json(parsedData)
+        }
+        );
+        }
+    });
+  }
 })
 
 app.get('/notes', (req, res) =>
